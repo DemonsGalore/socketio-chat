e13ba74b59096f75c0069bc29b7718791eae5034
Andreas Stopat <dev@demonsgalore.de>
2019-10-12 10:19:19 +0200

    set username before entering chat

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 TODO:
 - show user who joined/left chat
-- set username before joining
 - show all connected users
-- show typing problem if chatroom is change before 3sec after last typing
+- show typing problem if chatroom is changed before 3sec after last typing
+- toast notification (empty username, errors, etc)
+- check for unique usernames
--- client/src/pages/Chat/Chat.js
+++ client/src/pages/Chat/Chat.js
@@ -10,6 +10,7 @@ const Chat = () => {
   // local state
   const [message, setMessage] = useState('');
   const [username, setUsername] = useState('');
+  const [chatEntered, setChatEntered] = useState(false)
   const [activeTopic, setActiveTopic] = useState(topics[0]);
   const [timer, setTimer] = useState();
   
@@ -22,40 +23,55 @@ const Chat = () => {
     setTimer(setTimeout(() => {
       userStoppedTyping({ from: username, topic: activeTopic });
     }, 3000));
-  }
+  };
+
+  const enterChat = () => {
+    if (username !== '') {
+      setChatEntered(true);
+    }
+  };
 
   return (
     <StyledChat>
       <h1>Chat</h1>
-      <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-      <ul>
-        {topics.map(topic => (
-        <li key={topic} onClick={e => setActiveTopic(e.target.innerText)} className={topic === activeTopic ? 'active' : undefined}>
-          {topic}
-        </li>
-        ))}
-      </ul>
-      <h3>{activeTopic}</h3>
-      <div>
-        {activeChat.messages.map((chat, i) => (
-          <div key={i}>
-            <label><strong>{chat.from}</strong></label>
-            {chat.message}
+      {!chatEntered ?
+        <>
+          <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+          <button type="button" onClick={enterChat}>enter chat</button>
+        </>
+      :
+        <>
+          <ul>
+            {topics.map(topic => (
+            <li key={topic} onClick={e => setActiveTopic(e.target.innerText)} className={topic === activeTopic ? 'active' : undefined}>
+              {topic}
+            </li>
+            ))}
+          </ul>
+          <h3>{activeTopic}</h3>
+          <div>
+            {activeChat.messages.map((chat, i) => (
+              <div key={i}>
+                <label><strong>{chat.from}</strong></label>
+                {chat.message}
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-      {isUserTyping && <div><em>
-        {activeChat.typing.length > 1 ? (activeChat.typing.join(', ') + ' are typing') : (activeChat.typing[0] + ' is typing')}
-      </em></div>}
+          {isUserTyping && <div><em>
+            {activeChat.typing.length > 1 ? (activeChat.typing.join(', ') + ' are typing') : (activeChat.typing[0] + ' is typing')}
+          </em></div>}
 
-      <input type="text" value={message} onChange={e => setMessage(e.target.value)} onKeyUp={() => {
-        userTyping({ from: username, topic: activeTopic });
-        setTypingTimer();
-      }} />
-      <button type="button" onClick={() => {
-        sendChatAction({ from: username, message, topic: activeTopic });
-        setMessage('');
-      }}>send</button>
+          <input type="text" value={message} onChange={e => setMessage(e.target.value)} onKeyUp={() => {
+            userTyping({ from: username, topic: activeTopic });
+            setTypingTimer();
+          }} />
+          <button type="button" onClick={() => {
+            sendChatAction({ from: username, message, topic: activeTopic });
+            userStoppedTyping({ from: username, topic: activeTopic });
+            setMessage('');
+          }}>send</button>
+        </>
+      }
     </StyledChat>
   );
 }
